2594d0da28e738f612c57a741124f684835666d3
gyumong <rlaalsrb3559@naver.com>
2024-04-10 20:54:59 +0900

    feat: home page 임시 추가

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import MobileViewLayout from '@/commons/layouts/MobileViewLayout';
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  return <MobileViewLayout>{children}</MobileViewLayout>;
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function HomePage() {
+  return <div className="font-bold">hi</div>;
+}
