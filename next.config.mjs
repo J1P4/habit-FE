ba08794b5deb458e0572c6111e34b52860867922
gyumong <rlaalsrb3559@naver.com>
2024-04-10 20:54:25 +0900

    feat: / 로 접근시 home으로 리다이렉트

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        // / 에서 /home 으로 리디렉션
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true
+            }
+        ];
+    }
+};
 
 export default nextConfig;
