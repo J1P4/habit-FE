83a5a6d627b77cc26f8e311ec477d443335477cf
gyumong <rlaalsrb3559@naver.com>
2024-04-11 22:41:08 +0900

    feat: 버튼 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,10 @@
 'use client';
+import { Button } from '@/commons/components/ui/button';
+
 export default function HomePage() {
-  return <div className="font-bold">hi</div>;
+  return (
+    <div className="font-bold">
+      <Button>로그인</Button>
+    </div>
+  );
 }
