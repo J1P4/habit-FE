f9b776fb1054e4c1e9cd12cbf2b7fba2a47d6ed5
gyumong <rlaalsrb3559@naver.com>
2024-04-15 22:30:43 +0900

    feat: redirect page 처리

---
+++ src/app/redirect/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+/*
+ TODO redirect page token 처리 방식이 nextjs ssr을 통하여 인가코드로 token을 전달 받는 형식이 아닌 query string으로 받기 때문에 후에 수정 필요.
+ */
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const { push } = useRouter();
+  useEffect(() => {
+    const accessToken = searchParams.get('accessToken');
+    const role = searchParams.get('role');
+
+    if (!accessToken) return;
+    role === 'GUEST' ? push('/create-user-info') : push('/');
+  }, [searchParams]);
+
+  return <></>;
+}
