315ad62ecaf4565c93355296327cae0f1aa321da
gyumong <rlaalsrb3559@naver.com>
2024-04-15 22:29:54 +0900

    fix: 소셜 로그인 이동 함수 밖으로 이동

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,11 +3,13 @@ import KakaoLoginButton from '@/app/home/components/KakaoLoginButton';
 import FixedBottomWrapper from '@/commons/components/FixedBottomWrapper';
 
 export default function HomePage() {
+  const onClickKakao = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/oauth2/authorization/kakao`;
+  };
   return (
     <>
-      <div className="font-bold"></div>
       <FixedBottomWrapper>
-        <KakaoLoginButton />
+        <KakaoLoginButton onClick={onClickKakao} />
       </FixedBottomWrapper>
     </>
   );
