8ce4247c6f671d710791e8abde2e275632f4e450
gyumong <rlaalsrb3559@naver.com>
2024-04-15 21:22:22 +0900

    feat: mobile size layout

--- src/commons/layouts/MobileViewLayout.tsx
+++ src/commons/layouts/MobileViewLayout.tsx
@@ -1,9 +1,7 @@
 'use client';
 const MobileViewLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col items-center mx-auto max-w-[480px] mx-auto w-full h-screen">
-      {children}
-    </div>
+    <div className="flex flex-col mx-auto max-w-[480px] mx-auto w-full h-screen">{children}</div>
   );
 };
 
