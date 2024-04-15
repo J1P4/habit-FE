591179f555a211686cdda0ef9976f9d226b3ce22
gyumong <rlaalsrb3559@naver.com>
2024-04-15 22:28:21 +0900

    feat: 소셜 로그인 카카오 url 적용

--- src/app/home/components/KakaoLoginButton/index.tsx
+++ src/app/home/components/KakaoLoginButton/index.tsx
@@ -1,8 +1,13 @@
 const KakaoLoginButton = ({ onClick }: { onClick?: () => void }) => {
+  const onClickKakao = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/oauth2/authorization/kakao`;
+  };
   return (
     <button className="rounded-[20px] w-[322px] h-[65px] bg-[#FEE500] px-4 py-2" onClick={onClick}>
       <div className="flex items-center gap-[12px] w-full justify-center ">
-        <span className="text-lg font-bold">카카오 계정으로 시작하기</span>
+        <span className="text-lg font-bold" onClick={onClickKakao}>
+          카카오 계정으로 시작하기
+        </span>
       </div>
     </button>
   );
