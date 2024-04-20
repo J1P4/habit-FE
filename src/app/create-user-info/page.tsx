34e1fe6bb42a872ca0c8644128c5dab3198ced63
gyumong <rlaalsrb3559@naver.com>
2024-04-20 15:47:23 +0900

    feat: 불필요한 layout 제거

--- src/app/create-user-info/page.tsx
+++ src/app/create-user-info/page.tsx
@@ -4,9 +4,5 @@ import MobileViewLayout from '@/commons/layouts/MobileViewLayout';
 import StepContainer from '@/app/create-user-info/components/steps/step-container';
 
 export default function CreateUserInfoPage() {
-  return (
-    <MobileViewLayout>
-      <StepContainer />
-    </MobileViewLayout>
-  );
+  return <StepContainer />;
 }
