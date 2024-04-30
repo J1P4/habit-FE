3c4f693cc7c246050d1aed73c8f7fef35323a3e2
gyumong <rlaalsrb3559@naver.com>
2024-04-30 18:45:05 +0900

    feat: page padding layout 추가

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -2,11 +2,14 @@
 
 import MobileViewLayout from '@/commons/layouts/MobileViewLayout';
 import AuthLayout from '@/commons/layouts/auth-layout';
+import PagePaddingLayout from '@/commons/layouts/page-padding-layout';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthLayout>
-      <MobileViewLayout>{children}</MobileViewLayout>
+      <MobileViewLayout>
+        <PagePaddingLayout>{children}</PagePaddingLayout>
+      </MobileViewLayout>
     </AuthLayout>
   );
 }
