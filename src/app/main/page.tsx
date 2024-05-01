a065fde83fa1897d73fd2fac6df6a686c5f67529
gyumong <rlaalsrb3559@naver.com>
2024-05-01 14:16:35 +0900

    feat: 메인 페이지 유저 오늘 영양분 카드 컴포넌트 추가

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { Button } from '@/commons/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { getformatKRDate } from '@/commons/lib/getKRTime';
+import useGetUserNutrients from '@/app/main/api/queries/useGetUserNutrients';
+import Headline from '@/app/main/components/headline';
+import TodayUserNutritionAnalysisCard from '@/app/main/components/today-user-nutrition-analysis-card';
+import useGetTodayUserNutritionAnalysiscardData from '@/app/main/hooks/use-get-today-user-nutrition-analysiscard-data';
+
 export default function MainPage() {
-  return <>main</>;
+  const { summary, isSummaryLoading } = useGetTodayUserNutritionAnalysiscardData();
+
+  const { push } = useRouter();
+  console.log('summary', summary);
+  const goToTodayFood = () => {
+    push('/food-entry');
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <Headline title="오늘의 영양 분석 보고서 입니다" />
+      <TodayUserNutritionAnalysisCard summaryNutrients={summary} />
+      <Button onClick={goToTodayFood}>오늘 먹은 음식 입력하러 가기</Button>
+    </div>
+  );
 }
