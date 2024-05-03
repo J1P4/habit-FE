2f622d4966cd3ac43afe1f79c0654bae6db88b67
gyumong <rlaalsrb3559@naver.com>
2024-05-03 15:00:21 +0900

    feat: 영양분 상세 페이지 추가

--- src/app/detail-nutri-info/page.tsx
+++ src/app/detail-nutri-info/page.tsx
@@ -1,104 +1,107 @@
 'use client';
 
-import React from 'react';
-import { Chart as ChartJS, RadialLinearScale, ArcElement, Tooltip, Legend } from 'chart.js';
-import { PolarArea } from 'react-chartjs-2';
-
-ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+import React, { useState } from 'react';
+import NutriChart from '@/app/detail-nutri-info/components/nutri-chart';
+import NutriList from '@/app/detail-nutri-info/components/nutri-list';
+import {
+  DateRangeType,
+  NutrientData,
+} from '@/app/detail-nutri-info/api/queries/useGetUserNutriDetailHistory';
+import useDetailNutriList from '@/app/detail-nutri-info/hooks/useDetailNutriList';
+import { nutrientNameMapping } from '@/app/detail-nutri-info/constants/nutrientNameMapping';
 
 const DetailNutriInfoPage = () => {
-  const data = {
-    labels: ['탄수화물', '단백질', '지방'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [42, 39, 80],
-        backgroundColor: [
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  const [dateFilter, setDateFilter] = useState<DateRangeType>('DAY');
 
-  const options = {
-    scales: {
-      r: {
-        ticks: {
-          backdropColor: '#F4F9F3',
-        },
-        angleLines: {
-          display: false,
-        },
-        suggestedMin: 0,
-        suggestedMax: 100,
-      },
-    },
-  };
+  const { data, isGetUserNutriDetailHistoryLoading } = useDetailNutriList(dateFilter);
+
+  const carbohydrateRatio = Math.min(
+    (data?.totalData?.carbohydrate / (data?.essentialNutrition.carbohydrate ?? 1)) * 100,
+    100,
+  );
+
+  const proteinRatio = Math.min(
+    (data?.totalData?.protein / (data?.essentialNutrition.protein ?? 1)) * 100,
+    100,
+  );
+
+  const fatRatio = Math.min(
+    (data?.totalData?.fat / (data?.essentialNutrition.fat ?? 1)) * 100,
+    100,
+  );
+
+  // TODO Loading 상태일 때 로딩 컴포넌트 추가하기
+  if (isGetUserNutriDetailHistoryLoading) return <div>로딩중</div>;
   return (
     <>
       <div>
+        <div className="flex items-center py-4 px-4 gap-4">
+          <div
+            className={`cursor-pointer flex justify-center items-center border-blue-400 border-[1px]  w-[50px] rounded-lg ${dateFilter === 'DAY' ? 'bg-blue-500 text-white border-none' : 'bg-transparent'}`}
+            onClick={() => setDateFilter('DAY')}
+          >
+            일간
+          </div>
+          <div
+            className={`cursor-pointer flex justify-center items-center border-blue-400 border-[1px] rounded-lg w-[50px] ${dateFilter === 'WEEK' ? 'bg-blue-500 text-white border-none' : 'bg-transparent'}`}
+            onClick={() => setDateFilter('WEEK')}
+          >
+            주간
+          </div>
+          <div
+            className={`cursor-pointer flex justify-center items-center border-blue-400 border-[1px]   rounded-lg w-[50px] ${dateFilter === 'MONTH' ? 'bg-blue-500 text-white border-none' : 'bg-transparent'}`}
+            onClick={() => setDateFilter('MONTH')}
+          >
+            월간
+          </div>
+        </div>
         <div className="bg-[#F4F9F3] w-full  pt-4 flex flex-col items-center pb-6">
-          <div className="text-[#FF9385]">04.03 수</div>
+          <div className="text-[#FF9385]">{dateFilter}</div>
           <div className="mb-10" />
           <div className="flex gap-4 flex-col items-center w-full">
-            <PolarArea data={data} options={options} />
+            <NutriChart
+              carbohydrateRatio={carbohydrateRatio}
+              proteinRatio={proteinRatio}
+              fatRatio={fatRatio}
+            />
             <div className="mb-2" />
             <div className="flex justify-around w-full">
               <div className="flex flex-col items-center max-w-[60px] w-full">
                 <div className="text-[#FF9385]">탄수화물</div>
-                <div>42g</div>
+                <div>{Math.round(data?.totalData?.carbohydrate)}g</div>
               </div>
               <div className="flex flex-col items-center max-w-[60px] w-full">
                 <div className="text-[#FF9385]">단백질</div>
-                <div>39g</div>
+                <div>{Math.round(data?.totalData?.protein)}g</div>
               </div>
               <div className="flex flex-col items-center max-w-[60px] w-full">
                 <div className="flex flex-col items-center">
                   <div className="text-[#FF9385]">지방</div>
-                  <div>80g</div>
+                  <div>{Math.round(data?.totalData?.fat)}g</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center py-6 px-4">
-          <div>영양정보</div>
-          <div className="flex flex-col items-center w-full">
-            <div className="flex w-full justify-between  border-b-[1px] py-4 items-center">
-              <div>총 열량</div>
-              <div className="text-xl">2300 Kcal</div>
-            </div>
-            <ul className="w-full">
-              <li>
-                <div className="flex justify-between py-4">
-                  <div>탄수화물</div>
-                  <div>21g</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex justify-between py-4">
-                  <div>단백질</div>
-                  <div>6g</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex justify-between py-4">
-                  <div>지방</div>
-                  <div>6g</div>
-                </div>
-              </li>
-              <li>
-                <div className="flex justify-between py-4">
-                  <div>비타민A</div>
-                  <div>4g</div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <NutriList energy={data.totalData.energy}>
+          {Object.entries(data.totalData).map(([key, value]) => {
+            // 사용자가 섭취한 영양소의 양과 권장 영양소의 양을 가져옵니다.
+            const consumed = Math.round(value); // 사용자가 섭취한 양
+            const 권장영양소 = data.essentialNutrition;
+            const recommended = 권장영양소[key as keyof NutrientData]
+              ? Math.round(권장영양소[key as keyof NutrientData])
+              : '정보 없음'; // 권장량, 없다면 "정보 없음" 표시
+            return (
+              <NutriList.Item key={key}>
+                <div className=" w-[100px] ">{nutrientNameMapping[key] || key}</div>{' '}
+                {/* 영양소 이름 */}
+                <div className="w-[100px] text-center"> {consumed}</div>{' '}
+                {/* 사용자가 섭취한 영양소의 양 */}
+                <div className="w-[100px] text-center">{recommended}</div> {/* 권장 영양소의 양 */}
+              </NutriList.Item>
+            );
+          })}
+        </NutriList>
       </div>
     </>
   );
