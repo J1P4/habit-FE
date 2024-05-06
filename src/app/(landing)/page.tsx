627d1772f6892c5609640014eb2f7f784c16765b
gyumong <rlaalsrb3559@naver.com>
2024-05-06 13:41:15 +0900

    feat: habit logo 추가

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 import KakaoLoginButton from '@/app/(landing)/components/KakaoLoginButton';
 import FixedBottomWrapper from '@/commons/components/FixedBottomWrapper';
-
+import Image from 'next/image';
+import HabitLogo from '/public/assets/logo/habit_logo.png';
 export default function HomePage() {
   const onClickKakao = () => {
     window.location.href = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/oauth2/authorization/kakao`;
   };
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-dvh">
+      <Image src={HabitLogo} alt={'habit_logo.png'} className="mb-20" />
       <FixedBottomWrapper>
         <KakaoLoginButton onClick={onClickKakao} />
       </FixedBottomWrapper>
-    </>
+    </div>
   );
 }
