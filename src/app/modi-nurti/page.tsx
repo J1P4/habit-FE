6949e3b4bc70c7a9aac1ca74ecda8485c39c14b2
gyumong <rlaalsrb3559@naver.com>
2024-05-07 21:13:14 +0900

    fix: 음식 수정 ui 수정

--- src/app/modi-nurti/page.tsx
+++ src/app/modi-nurti/page.tsx
@@ -26,8 +26,16 @@ export default function Page() {
           <div className="flex w-full py-4 border-b-2">{food.date}</div>
           {food.foods.map((food) => (
             <div key={food.historyId}>
-              <div className="flex py-4" onClick={() => onClickItem(food.historyId)}>
-                {food.foodName}
+              <div
+                className="flex flex-col py-4 border-[1px] my-4 rounded-md cursor-pointer hover:bg-gray-100  px-2 w-full"
+                onClick={() => onClickItem(food.historyId)}
+              >
+                <div className="text-[16px] font-bold">{food.foodName}</div>
+                <div className="flex mt-4 justify-between text-[12px] ">
+                  <div>탄수화물 : {Math.round(food.carbohydrate)}g</div>
+                  <div>단백질 : {Math.round(food.protein)}g</div>
+                  <div>지방 : {Math.round(food.fat)}g</div>
+                </div>
               </div>
             </div>
           ))}
