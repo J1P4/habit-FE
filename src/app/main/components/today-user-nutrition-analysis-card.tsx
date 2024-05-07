76d7e255b9066841be2f6fe4ee5ada55020d344e
enunsnv <seoej0315@naver.com>
2024-05-08 00:55:28 +0900

    feat: main 영양소 스타일 수정

--- src/app/main/components/today-user-nutrition-analysis-card.tsx
+++ src/app/main/components/today-user-nutrition-analysis-card.tsx
@@ -24,28 +24,28 @@ const TodayUserNutritionAnalysisCard = ({
   };
   return (
     <div className="w-full flex flex-col bg-[#F4F9F3] rounded-[30px] py-4 px-5 items-center">
-      <div className="flex gap-5 justify-center">
-        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[90px]">
+      <div className="flex gap-4 justify-center mt-2">
+        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[95px] h-[80px]">
           <div className="text-[#FF9385] font-bold ">탄수화물</div>
-          <div className="text-[12px] text-center">
+          <div className="text-[12px] text-center font-bold">
             {carbohydrate} / {essentialCarbohydrate}g
           </div>
         </div>
-        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[90px]">
+        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[95px]">
           <div className="text-[#FF9385] font-bold ">단백질</div>
-          <div className="text-[12px] text-center">
+          <div className="text-[12px] text-center font-bold">
             {protein} / {essentialProtein}g
           </div>
         </div>
-        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[90px]">
+        <div className="bg-white px-3 py-2 rounded-[10px] flex flex-col items-center gap-3 w-full min-w-[95px]">
           <div className="text-[#FF9385] font-bold ">지방</div>
-          <div className="text-[12px] text-center">
+          <div className="text-[12px] text-center font-bold">
             {fat} / {essentialFat}g
           </div>
         </div>
       </div>
-      <div className="mt-10" />
-      <Button className="w-[200px] bg-[#FF9385] rounded-[10px]" onClick={gotoDetailPage}>
+      <div className="mt-8" />
+      <Button className=" w-[200px] bg-[#FF9385] rounded-[10px]" onClick={gotoDetailPage}>
         자세히 보기
       </Button>
     </div>
