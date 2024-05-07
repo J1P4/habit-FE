e651e93872547111e28fb0e2b8f576b24fe3ab35
enunsnv <seoej0315@naver.com>
2024-05-07 16:15:40 +0900

    feat: 음식 상세 정보 추가

--- src/app/recommend-food-info/components/Food.tsx
+++ src/app/recommend-food-info/components/Food.tsx
@@ -5,8 +5,16 @@ import axios from 'axios';
 import Link from 'next/link';
 import { fetcher } from '@/commons/apis/fetcher';
 import RestaurantIcon from './restaurant-icon';
+import { Button } from '@/commons/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 const FoodComponent = () => {
+  const router = useRouter();
+
+  const goToTodayFood = () => {
+    router.push('/food-entry');
+  };
+
   const [foodlist, setFoodList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -19,7 +27,7 @@ const FoodComponent = () => {
         if (data && data.foodlist) {
           setFoodList(data.foodlist);
         } else {
-          setError('No food data available');
+          setError('오늘 먹은 음식을 입력해주세요!');
         }
         setLoading(false);
       } catch (error) {
@@ -28,23 +36,34 @@ const FoodComponent = () => {
       }
     };
 
+   
     fetchFoodList();
   }, []);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  if (error) {
+  return (
+    <div>
+      <div className='w-full text-center font-bold my-[50px]'>오늘 먹은 음식이 없어요! 입력해주세요 😊</div>
+      <Button className="w-full bg-[#FF9385] rounded-[10px] py-6" onClick={goToTodayFood}>
+        오늘 먹은 음식 입력하러 가기
+      </Button>
+    </div>
+  );
+  }
+  
 
   return (
     <div>
       {foodlist.map((food, index) => (
         <div key={index} className="flex items-center bg-gray-100 mx-10 my-3 p-5 rounded-lg">
-          <div className="flex flex-col ">
-            <p className="text-sm text-[#6CB663]">{food.energy}kcal</p>
-            <h4 className="text-lg font-semibold">{food.name}</h4>
+          <div className="flex flex-col p-1">
+            <p className="text-md text-[#6CB663] font-semibold">{food.kcal}kcal</p>
+            <h4 className="text-lg font-semibold py-1">{food.name}</h4>
             <div className="flex">
-              <p className="text-sm mr-2 text-[#767676]">탄 {food.moisture}g | </p>
-              <p className="text-sm mr-2 text-[#767676]">단 {food.protein}g | </p>
-              <p className="text-sm mr-2 text-[#767676]">지 {food.fat}g</p>
+              <p className="text-sm mr-2 text-[#767676]">탄수 {food.carbohydrate}g &nbsp; | </p>
+              <p className="text-sm mr-2 text-[#767676]">단백질 {food.protein}g &nbsp; | </p>
+              <p className="text-sm mr-2 text-[#767676]">지방 {food.fat}g &nbsp;</p>
             </div>
           </div>
 
